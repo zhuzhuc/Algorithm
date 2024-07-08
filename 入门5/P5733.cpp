29241b951e8293f5dc412c6617e34c1306c00da5
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-07-08 17:27:16 +0800

    Luogu

---
+++ 入门5/P5733.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+
+const int maxn = 2e5 + 2;
+const int maxm = 62;
+void solve() {
+    string s;
+    cin >> s;
+    for (char &c : s)
+    {
+        c = toupper(c);
+    }
+    cout << s << "\n";
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
