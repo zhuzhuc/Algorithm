637b65e2a46a674a478fc7d9ddfceaf2b369b30d
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-07-10 15:48:20 +0800

    Luogu

---
+++ 入门6/P5739.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+
+const int maxn = 2e5 + 2;
+const int maxm = 62;
+bool isLeapYear(int year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true; 
+    if (n % 2 == 0) return false; 
+    for (int i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) return false;}
+    return true;
+}
+unsigned long long factorial(int n) {
+    if (n == 0) {
+        return 1; 
+    }
+    return n * factorial(n - 1);
+}
+void solve() {
+    int n;
+    cin >> n;
+    int ans = 0;
+    if (n> 0)
+    {
+    	ans = factorial(n);
+    }
+    cout << ans << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
