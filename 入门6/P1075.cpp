2b03310cc1af45bb5f38f2b82e1b0b74a11f77d3
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-07-10 14:39:36 +0800

    Luogu

---
+++ 入门6/P1075.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+#define ll Long Long
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+
+const int maxn = 2e5 + 2;
+const int maxm = 62;
+
+void solve() {
+    int n;
+    cin >> n;
+
+    int i = 2;
+    while (i <= sqrt(n)) {
+        if (n % i == 0) break;
+        i++;
+    }
+    
+    cout << n / i << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
