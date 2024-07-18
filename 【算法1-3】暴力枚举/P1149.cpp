c80bc2869fb23167c9a3999fc285239346c1a297
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-07-18 14:35:04 +0800

    Luogu

---
+++ 【算法1-3】暴力枚举/P1149.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+using namespace std;
+
+#define ll long long
+#define ff first
+#define ss second
+#define pb push_back
+#define eb emplace_back
+
+const int maxn = 2e5 + 2;
+const int maxm = 62;
+
+bool isLeapYear(int year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if (year % 100 == 0) {
+        return false;
+    } else if (year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true; 
+    if (n % 2 == 0) return false; 
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+string multiply(string num1, int num2) {
+    int carry = 0;
+    for (int i = num1.size() - 1; i >= 0; --i) {
+        int product = (num1[i] - '0') * num2 + carry;
+        num1[i] = (product % 10) + '0';
+        carry = product / 10;
+    }
+    while (carry) {
+        num1.insert(num1.begin(), (carry % 10) + '0');
+        carry /= 10;
+    }
+    return num1;
+}
+
+string factorial(int n) {
+    string result = "1";
+    for (int i = 2; i <= n; ++i) {
+        result = multiply(result, i);
+    }
+    return result;
+}
+
+int count_digit(const string& number, char digit) {
+    return count(number.begin(), number.end(), digit);
+}
+int fire[10] = {6,2,5,5,4,5,6,3,7,6};
+
+
+int count(int num){
+	if (num == 0)
+	{
+		return fire[0];
+	}
+	int count_a = 0;
+	while(num > 0){
+		count_a += fire[num%10];
+		num /= 10;
+	}
+
+	return count_a;
+}
+void solve() {
+    int t;
+    cin >> t;
+
+    t -= 4;
+    
+    
+    int result = 0;
+
+    for (int i = 0; i <= 1000; ++i)
+    {
+    	int afire = count(i);
+    	for (int j = 0; j <= 1000; ++j)
+    	{
+    		int bfire = count(j);
+    		int c = i + j;
+    		int cfire = count(c);
+
+
+    		if (afire + bfire + cfire == t)
+    		{
+    			result++;
+    		}
+    	}
+    }
+    cout << result << "\n";
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+    return 0;
+}
