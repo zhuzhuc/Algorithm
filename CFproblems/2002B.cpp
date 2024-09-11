6920cfc2d0b1db79e14637fb20cc179713e1be31
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-09-11 22:33:56 +0800

    CF_daily

---
+++ CFproblems/2002B.cpp
@@ -0,0 +1,138 @@
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#ifndef _GLIBCXX_NO_ASSERT
+#include<cassert>
+#endif
+#include<cctype>
+#include<cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+ 
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+//#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define ll long long
+#define dou double
+using namespace std;
+
+typedef unsigned long long ull;
+#define ms(s) memset(s, 0, sizeof(s))
+const int inf = 0x3f3f3f3f;
+#define LOCAL
+ 
+
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int n;
+        cin >> n;
+        vector<int> a, b;
+
+        for (int i = 1; i <= n; i++) {
+            int x; 
+            cin >> x;
+            a.push_back(x);
+        }
+        for (int i = 1; i <= n; i++) {
+            int x;
+            cin >> x;
+            b.push_back(x);
+        }
+
+        if (a == b) {
+            cout << "Bob" << endl;
+            continue;
+        }
+
+        reverse(a.begin(), a.end());
+
+        if (a == b) {
+            cout << "Bob" << endl;
+            continue;
+        }
+
+        cout << "Alice" << endl;
+    }
+
+    return 0;
+}
