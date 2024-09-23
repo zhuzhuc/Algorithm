e7b08cfedb8815940073875ce8456450dfc23097
zz_milk <139876804+zhuzhuc@users.noreply.github.com>
2024-09-23 16:24:02 +0800

    CSP_solve

---
+++ CSP题解/第16次/1.cpp
@@ -0,0 +1,138 @@
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#ifndef _GLIBCXX_NO_ASSERT
+#include<cassert>
+#endif
+#include<cctype>
+#include<cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+ 
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+//#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define ll long long
+#define dou double
+using namespace std;
+
+typedef unsigned long long ull;
+#define ms(s) memset(s, 0, sizeof(s))
+const int inf = 0x3f3f3f3f;
+#define LOCAL
+ 
+ 
+ 
+void solve(){
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++){
+		cin >> v[i];
+	}
+	double mid = 0;
+	int max_C = *max_element(v.begin(), v.end());
+	int min_C = *min_element(v.begin(), v.end());
+	if(n % 2 == 0){
+		for(int j = n/2-1; j <  n/2;j++){
+			mid = (v[n/2-1] + v[n/2]) / 2.0;
+		}
+	}else{
+		mid = v[n/2];
+	}
+	cout << fixed << setprecision(1);
+	cout << max_C  << " ";//<< round(mid*10)/10 << " " << min_C << endl;
+	if(mid ==(int)mid){
+		cout << (int)mid << " ";
+	}else{
+		cout << fixed << setprecision(1) << mid << " ";
+	}
+	cout << min_C;
+
+}
+// 1 2 3 4 5 6 7
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+                    
+    solve();
+    return 0;
+}
