00fe7bd905025d22358191a345b746d488b289e4
zhuzhuc <1727643988@qq.com>
2025-03-22 16:04:26 +0800

    update

---
+++ CFproblems/2082B.cpp
@@ -0,0 +1,134 @@
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#ifndef _GLIBCXX_NO_ASSERT
+#include<cassert>
+#endif
+#include<cctype>
+#include<cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+ 
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+//#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+ 
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#define ll long long
+#define dou double
+using namespace std;
+
+typedef unsigned long long ull;
+#define ms(s) memset(s, 0, sizeof(s))
+const int inf = 0x3f3f3f3f;
+#define LOCAL
+ 
+ll F(ll x,ll n){
+	while (n --){
+		if (!x) return x;
+		x = (x >> 1);
+	}
+	return x;
+}
+ll C(ll x,ll n){
+	while (n --){
+		if (x <= 1) return x;
+		x = ((x + 1) >> 1);
+	}
+	return x;
+}
+ 
+void solve(){
+    ll x,n,m;
+    cin >> x >> n >> m;
+    printf("%lld %lld\n",F(C(x,m),n),C(F(x,n),m));
+}
+
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+    }           
+    // solve();
+    return 0;
+}
